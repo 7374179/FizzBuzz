164448b6b2efe851d188319794b0a1d324331265
Taehyuk Chung <tchung60@my.bcit.ca>
2024-04-05 11:20:41 -0700

    created fizzbuzz

---
+++ FizzBuzzLibrary/FizzBuzzLogic.cs
@@ -0,0 +1,43 @@
+﻿namespace FizzBuzzLibrary;
+
+public class FizzBuzzLogic
+{
+    public string? GetOutput(int? number)
+    {
+        if (number < 0)
+        {
+            throw new Exception("Number cannot be negative.");
+        }
+        if (number > 100)
+        {
+            throw new Exception("Number cannot be greater than 100.");
+        }
+        if (number is null)
+        {
+            throw new Exception("Number cannot be null.");
+        }
+
+        string output = string.Empty;
+        for(int i = 1; i <= number; i++)
+        {
+            if(i % 3 == 0 && i % 5 == 0)
+            {
+                output += "FizzBuzz ";
+            }
+            else if(i % 3 == 0)
+            {
+                output += "Fizz ";
+            }
+            else if(i % 5 == 0)
+            {
+                output += "Buzz ";
+            }
+            else
+            {
+                output += $"{i} ";
+            }
+        }
+        return output;
+
+    }
+}
---
+++ FizzBuzzMvc/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using FizzBuzzMvc.Models;
+using FizzBuzzLibrary;
+
+namespace FizzBuzzMvc.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult FizzBuzz(int? number)
+    {
+        FizzBuzzLogic fb = new FizzBuzzLogic();
+        string? output = fb.GetOutput(number);
+        ViewBag.Output = output;
+        return View("Index");
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
